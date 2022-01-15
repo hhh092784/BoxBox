8ca2426020582b5638456938a3a6a0a728ad7924
hhh092784 <hhh092784@gmail.com>
2022-01-15 13:48:19 +0900

    localhost success

---
+++ boxbox/src/main/java/com/example/boxbox/BoxboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.boxbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxboxApplication.class, args);
+	}
+
+}
